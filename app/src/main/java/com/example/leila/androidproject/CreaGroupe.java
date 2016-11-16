31b51c6aeda6dd8624623074793dc994e368950b
skownar <skownar27@gmail.com>
2016-11-16 12:07:14 +0100

    Envoi fichier

--- app/src/main/java/com/example/leila/androidproject/CreaGroupe.java
+++ app/src/main/java/com/example/leila/androidproject/CreaGroupe.java
@@ -9,6 +9,7 @@ public class CreaGroupe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crea_groupe);
+
     }
 
 
