893917d2195f76a4d83d7a2756d8e4a9e47cbcd0
skownar <skownar27@gmail.com>
2016-11-16 13:55:14 +0100

    Envoi fichier

--- app/src/main/java/DAO/DAO.java
+++ app/src/main/java/DAO/DAO.java
@@ -1,15 +1,24 @@
 package DAO;
 
+import android.net.Uri;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.uri.UriBuilderImpl;
+
+import java.net.URI;
 import java.util.ArrayList;
+
+import javax.ws.rs.core.UriBuilder;
 
 /**
  * Created by lafer on 16-11-16.
  */
 
 public interface DAO<T> {
-    public ArrayList<T> readAll();
-    public T readById();
-    public int create();
-    public boolean delete();
-    public boolean update();
+    ArrayList<T> readAll();
+    T readById();
+    int create();
+    boolean delete();
+    boolean update();
 }
--- app/src/main/java/DAO/groupeDAO.java
+++ app/src/main/java/DAO/groupeDAO.java
@@ -1,14 +1,29 @@
 package DAO;
 
 import com.example.leila.androidproject.Groupe;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
 
+import java.net.URI;
 import java.util.ArrayList;
+
+import javax.ws.rs.core.UriBuilder;
 
 /**
  * Created by lafer on 16-11-16.
  */
 
 public class groupeDAO implements DAO<Groupe> {
+    Client c;
+    URI uri;
+    WebResource service;
+    ArrayList<Groupe> al;
+    public groupeDAO() {
+        c = new Client();
+        uri = UriBuilder.fromUri("test").build();
+        service = c.resource(uri);
+        al = new ArrayList<>();
+    }
 
     @Override
     public ArrayList<Groupe> readAll() {
@@ -19,6 +34,7 @@ public class groupeDAO implements DAO<Groupe> {
 
     @Override
     public boolean delete() {
+
         return true;
     }
 
