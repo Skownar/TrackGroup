9cc414e6aa58561f67c7589a57c6d87c5fb639a5
leeeib <leeilaab@hotmail.fr>
2016-11-17 08:39:17 +0100

    Ajout membre, et layouts de creation de groupe

---
+++ app/src/main/java/com/example/leila/androidproject/Membre.java
@@ -0,0 +1,107 @@
+package com.example.leila.androidproject;
+
+/**
+ * Created by Leïla on 16-11-16.
+ */
+
+public class Membre {
+    int id_membre;
+    String nom;
+    String prenom;
+    String email;
+    String username;
+    String pwd;
+    String localisation;
+    int groupe_choisi;
+
+    public Membre(){}
+
+    public Membre(int id_membre, String nom, String prenom, String email, String username, String pwd, String localisation,int groupe_choisi) {
+        this.id_membre = id_membre;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.username = username;
+        this.pwd = pwd;
+        this.localisation = localisation;
+        this.groupe_choisi=groupe_choisi;
+    }
+
+    public int getId_membre() {
+        return id_membre;
+    }
+
+    public void setId_membre(int id_membre) {
+        this.id_membre = id_membre;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getLocalisation() {
+        return localisation;
+    }
+
+    public void setLocalisation(String localisation) {
+        this.localisation = localisation;
+    }
+
+    public int getGroupe_choisi() {
+        return groupe_choisi;
+    }
+
+    public void setGroupe_choisi(int groupe_choisi) {
+        this.groupe_choisi = groupe_choisi;
+    }
+
+    @Override
+    public String toString() {
+        return "Membre{" +
+                "id_membre=" + id_membre +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", email='" + email + '\'' +
+                ", username='" + username + '\'' +
+                ", pwd='" + pwd + '\'' +
+                ", localisation='" + localisation + '\'' +
+                ", groupe_choisi=" + groupe_choisi +
+                '}';
+    }
+}
