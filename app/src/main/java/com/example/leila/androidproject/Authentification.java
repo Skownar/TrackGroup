3484dcf49b05d44613f2052b0bb3b8605e594608
skownar <skownar27@gmail.com>
2016-11-18 15:38:00 +0100

    creaGroupe Maj

--- app/src/main/java/com/example/leila/androidproject/Authentification.java
+++ app/src/main/java/com/example/leila/androidproject/Authentification.java
@@ -1,14 +1,21 @@
 package com.example.leila.androidproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Authentification extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_authentification);
+        Button b = (Button) findViewById(R.id.btnSinscrire);
+        b.setOnClickListener(v -> {
+            Intent t = new Intent(this,CreaMembre.class);
+            startActivity(t);
+        });
+
     }
 }
